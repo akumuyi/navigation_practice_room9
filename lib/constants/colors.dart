0408105c2a40b38b7400658c9702096146fd856c
UMUTONI Kevine <138827957+simplykevine@users.noreply.github.com>
2025-02-07 21:11:39 +0200

    DONE

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,1 +1,9 @@
-// Your code goes here
+import 'package:flutter/material.dart';
+
+final List<Color> productColorList = [
+  Colors.blue,    
+  Color.fromARGB(255, 77, 219, 82),   // For Laptop
+  Color.fromARGB(255, 212, 197, 59),  // For Tablet
+  Color.fromARGB(255, 194, 90, 52),   // For Pen Drive
+  Color.fromARGB(255, 93, 222, 226),   // For Floppy Drive
+];
